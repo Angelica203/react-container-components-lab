15cc77602b1bdc7b9de79dc2ee48b4219d6ac9a7
github username <github email address>
2021-11-23 13:13:56 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,13 @@
 // Code MovieReviews Here
+import React, { Component } from 'react'
+import LatestMovieReviewsContainer from './LatestMovieReviewsContainer'
+
+
+// const MovieReviews =({reviews}) => (
+//     <div className="review-list">
+//         <{reviews.map((review)}
+           
+//     </div>
+// )
+
+const MovieReviews = ({reviews}) => <div className="review-list">{reviews.map(Review)}</div>
